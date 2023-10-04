6112e966f8b2586c2f282109cb792d4e2ad6aeb1
Konashuk <konan4ukv@gmail.com>
2023-10-04 19:04:37 +0300

    remove reeors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -22,7 +23,7 @@ function loadMore() {
   searchImage();
 }
 
-function searchImage() {
+async function searchImage() {
   const inputValue = inputEl.value;
   const BASE_URL = 'https://pixabay.com/api/?';
   const KEY = '39751957-699f95fae17e6e2f35ebbbaf7';
@@ -30,23 +31,23 @@ function searchImage() {
   const ORIENT_PHOTO = 'horizontal';
   const WHAT_SEARCH = inputValue;
 
-  fetch(
-    `${BASE_URL}key=${KEY}&image_type=${TYPE_SEARCH}&orientation=${ORIENT_PHOTO}&safesearch=true&q=${WHAT_SEARCH}&per_page=40&page=${page}`
-  )
+  await axios
+    .get(
+      `${BASE_URL}key=${KEY}&image_type=${TYPE_SEARCH}&orientation=${ORIENT_PHOTO}&safesearch=true&q=${WHAT_SEARCH}&per_page=40&page=${page}`
+    )
     .then(response => {
-      if (!response.ok) {
+      if (response.status !== 200) {
         throw new Error(response.statusText);
       }
-      return response.json();
+      return response.data;
     })
     .then(data => {
       totalHits = data.totalHits;
-
       createMarkupCard(data);
     })
-
     .catch(error => {
-      console.log(error.statusText);
+      console.log(error);
+      Notiflix.Notify.failure('Sorry, error ...');
     });
 }
 
@@ -62,9 +63,17 @@ function createMarkupCard(data) {
 
   const markup = hits
     .map(
-      ({ webformatURL, tags, likes, views, comments, downloads }) => `
+      ({
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+        largeImageURL,
+      }) => `
         <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" width='300' height='200' />
+          <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width='300' height='200' /></a>
           <div class="info">
             <p class="info-item">
               <b>Likes: ${likes}</b>
@@ -91,11 +100,15 @@ function createMarkupCard(data) {
   loadMoreBtn.classList.remove('is-hidden');
 
   if (page * 40 >= totalHits) {
-    loadMoreBtn.classList.add('is-hidden'); // Ховаємо кнопку
+    loadMoreBtn.classList.add('is-hidden');
     Notiflix.Notify.info(
       "We're sorry, but you've reached the end of search results."
     );
   } else {
     loadMoreBtn.classList.remove('is-hidden');
   }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: `alt`,
+  });
 }
