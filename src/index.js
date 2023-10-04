ad4205a11e5fe3162bee4b51488769d7b9fc45b8
Konashuk <konan4ukv@gmail.com>
2023-10-04 18:43:41 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,64 +1,101 @@
-import axios from 'axios';
-import Notiflix, { Loading } from 'notiflix';
+import Notiflix from 'notiflix';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
-const galeryEl = document.querySelector('.gallery');
-formEl.addEventListener('submit', searchImage);
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-function searchImage(ev) {
-  ev.preventDefault();
-  const ipnutText = inputEl.value;
+formEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+  searchImage();
+});
+
+loadMoreBtn.addEventListener('click', loadMore);
+
+let page = 1;
+
+function loadMore() {
+  page += 1;
+  searchImage();
+}
+
+function searchImage() {
+  const inputValue = inputEl.value;
   const BASE_URL = 'https://pixabay.com/api/?';
   const KEY = '39751957-699f95fae17e6e2f35ebbbaf7';
   const TYPE_SEARCH = 'photo';
   const ORIENT_PHOTO = 'horizontal';
-  const WHAT_SHEARCH = ipnutText;
+  const WHAT_SEARCH = inputValue;
 
-  axios
-    .get(
-      `${BASE_URL}key=${KEY}&image_type=${TYPE_SEARCH}&orientation=${ORIENT_PHOTO}&safesearch=true&q=${WHAT_SHEARCH}`
-    )
-    .then(resp => createMarcupCard(resp))
-    .catch(err => console.log(err.statusText));
+  fetch(
+    `${BASE_URL}key=${KEY}&image_type=${TYPE_SEARCH}&orientation=${ORIENT_PHOTO}&safesearch=true&q=${WHAT_SEARCH}&per_page=40&page=${page}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      totalHits = data.totalHits;
+
+      createMarkupCard(data);
+    })
+
+    .catch(error => {
+      console.log(error.statusText);
+    });
 }
 
-function createMarcupCard(resp) {
-  const data = resp.data.hits;
+function createMarkupCard(data) {
+  const hits = data.hits;
 
-  if (data.length === 0) {
+  if (hits.length === 0) {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     return;
   }
-  const markup = data.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => `
-          <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width='300' height='200' />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads${downloads}</b>
-    </p>
-  </div>
-</div>`
-  );
-  return (galeryEl.innerHTML = markup.join(''));
+
+  const markup = hits
+    .map(
+      ({ webformatURL, tags, likes, views, comments, downloads }) => `
+        <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width='300' height='200' />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: ${downloads}</b>
+            </p>
+          </div>
+        </div>`
+    )
+    .join('');
+
+  if (page === 1) {
+    galleryEl.innerHTML = markup;
+  } else {
+    galleryEl.insertAdjacentHTML('beforeend', markup);
+  }
+  loadMoreBtn.classList.remove('is-hidden');
+
+  if (page * 40 >= totalHits) {
+    loadMoreBtn.classList.add('is-hidden'); // Ховаємо кнопку
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  } else {
+    loadMoreBtn.classList.remove('is-hidden');
+  }
 }
